7f49a1fdb3b9bf6d7a05e943a9b05665f604e051
vinivogt <viniciusvogt02@gmail.com>
2025-09-03 14:28:56 -0300

    divided files v3
    update

--- docs/script.js
+++ docs/script.js
@@ -11,6 +11,7 @@ setInterval(() => {
     flight.style.top = `${Math.random() * 80 + 10}%`;
     flight.style.left = `${Math.random() * 80 + 10}%`;
     radar.appendChild(flight);
+
     // Remove some flights after a while
     if (radar.children.length > 8) {
         radar.removeChild(radar.children[1]);
